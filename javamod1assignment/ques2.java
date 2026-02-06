6d7796efc3a8ddad659ca26f5aea0d50d7df7dda
thx-manas <thxmanassingh@gmail.com>
2026-02-06 23:49:53 +0530

    Update ques2.java

--- javamod1assignment/ques2.java
+++ javamod1assignment/ques2.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
-class Salary{
+class salary{
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        double salary;
-        salary = sc.nextDouble();
-       
-        if(salary >= 30000){
-            salary = salary + (0.20 * salary) + (0.15 * salary);
-        }
-        else if(salary >= 20000){
-            salary = salary + (0.10 * salary) + (0.08 * salary);
-        }
+        System.out.println("enter the basic salary");
+        double basic = sc.nextDouble();
+
+        if(basic >= 30000){
+            basic = basic + (0.20 * basic) + (0.15 * basic);
+        } 
         else{
-            salary = salary - (0.12 * salary);
+            basic = basic + ( 0.10*basic ) + (0.08*basic) - ( 0.12*basic);
         }
-        System.out.println("Net salary = " + salary);
+
+        System.out.println("Your monthly salary is: " + basic);
+        sc.close();
     }
 }
